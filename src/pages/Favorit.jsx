568b8efb4cbdb5e0de35d20e39d697d1f7f20ef1
adikarais <103338766+adikarais@users.noreply.github.com>
2024-12-15 14:23:42 +0700

    Update Favorit.jsx

--- src/pages/Favorit.jsx
+++ src/pages/Favorit.jsx
@@ -5,7 +5,6 @@ function Favorit () {
   return (
     <>
     <head>
-        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Musetix - Favorit</title>
       </head>
